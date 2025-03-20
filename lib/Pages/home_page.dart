fd1b21a548033d7f91b9504e49803a595ad9b879
shresthasameerman <shresthasameerman@gmail.com>
2025-03-20 17:22:16 +0545

    To Do App is in the 4nd stage

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -13,7 +13,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-final _controller = TextEditingController();
+  final _controller = TextEditingController();
 
   List toDoList=[
     ["watch tutorial",false],
@@ -30,29 +30,29 @@ final _controller = TextEditingController();
 
   }
 
-Future<void> scheduleNotification(String taskName) async {
-  var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-    'your_channel_id',
-    'your_channel_name',
-    'your_channel_description',
-    importance: Importance.max,
-    priority: Priority.high,
-    ticker: 'ticker',
-  );
-  var iOSPlatformChannelSpecifics = IOSNotificationDetails(); // Correct class name
-  var platformChannelSpecifics = NotificationDetails(
-      android: androidPlatformChannelSpecifics, iOS: iOSPlatformChannelSpecifics);
-  await flutterLocalNotificationsPlugin.show(
-    0,
-    'Incomplete Task',
-    'You have an incomplete task: $taskName',
-    platformChannelSpecifics,
-    payload: 'item x',
-  );
-}
+  Future<void> scheduleNotification(String taskName) async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      'your_channel_description',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+    );
+    var iOSPlatformChannelSpecifics = DarwinNotificationDetails(); // Correct class name
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics, iOS: iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Incomplete Task',
+      'You have an incomplete task: $taskName',
+      platformChannelSpecifics,
+      payload: 'item x',
+    );
+  }
 
   // save new task
-void saveNewTask(){
+  void saveNewTask(){
     setState(() {
       toDoList.add([
         _controller.text, false
@@ -60,21 +60,21 @@ void saveNewTask(){
       _controller.clear();
     });
     Navigator.of(context).pop();
-}
+  }
 
 
 //create a new task for save and cancel
   void createNewTask(){
     showDialog(
-        context: context,
-        builder: (context){
-          return DialogBox(
-            controller: _controller,
-            onSave: saveNewTask,
-            onCancel: () => Navigator.of(context).pop(),
+      context: context,
+      builder: (context){
+        return DialogBox(
+          controller: _controller,
+          onSave: saveNewTask,
+          onCancel: () => Navigator.of(context).pop(),
 
-          );
-        },
+        );
+      },
     );
 
   }
@@ -90,29 +90,29 @@ void saveNewTask(){
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('Taskora'),
-        elevation: 0,
-      ),
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text('Taskora'),
+          elevation: 0,
+        ),
 
-      floatingActionButton: FloatingActionButton(onPressed:
+        floatingActionButton: FloatingActionButton(onPressed:
         createNewTask,
-        child: Icon(Icons.add),
-      ),
+          child: Icon(Icons.add),
+        ),
 
-      body: ListView.builder(
-        itemCount: toDoList.length,
-        itemBuilder: (context,index){
-          return ToDoTile(
-            taskName: toDoList[index][0],
-            taskCompleted: toDoList[index][1],
-            onChanged: (value) => checkBoxChanged(value ?? false, index),
-            deleteFunction:(context) => DeleteTask(index),
-          );
+        body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (context,index){
+            return ToDoTile(
+              taskName: toDoList[index][0],
+              taskCompleted: toDoList[index][1],
+              onChanged: (value) => checkBoxChanged(value ?? false, index),
+              deleteFunction:(context) => DeleteTask(index),
+            );
 
-        },
-      )
+          },
+        )
     );
   }
 }
